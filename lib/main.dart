1d78fcec202d347da71efc1d599c0fec6810eab3
pengda.sun <pengda.sun@hanshow.com>
2025-02-25 22:26:41 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'dart:ffi';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String tip = '';
+
+  @override
+  void initState() {
+    super.initState();
+    String mode = kReleaseMode ? 'release' : 'debug';
+    try {
+      DynamicLibrary.open('aes256_lib.so');
+      tip = "✅ Load success in $mode mode";
+    } catch (e) {
+      print(e);
+      tip = "❌ Load failed in $mode mode:\n${e.toString()}";
+    }
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Text(
+                tip,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
